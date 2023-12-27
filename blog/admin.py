5f0672d81bfc4e12ff31a48f612e171583ef96d6
masayan08238888 <masayan08238888@gmail.com>
2023-12-27 10:46:56 +0900

    Initial Commit

---
+++ blog/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from blog.models import Category, Blog
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display=('id','title', 'postdate','category')
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display=('id', 'category_name')
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Blog, BlogAdmin)
